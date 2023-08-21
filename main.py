ff2d62c5f59cc6b6bbb58abe143e51a945b18203
atsuki <csatsfapsfu2930@gmail.com>
2023-08-20 23:25:51 -0700

    organized code

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from tkinter import *
+from tkinter import messagebox
 
-
-def main():
+def create_interface():
     top = Tk()
 
     top.title("YouTube to Spotify Playlist")
     top.geometry("450x300")
 
     # label for URL input
+    Label(top, text="Enter YT URL").place(x=160, y=60)
 
-    youtube_link = Label(top, text="Enter YT URL").place(x=160, y=60)
+    # Create the Entry widget and assign it to the variable
+    youtube_link_input_area = Entry(top, width=30)
+    youtube_link_input_area.place(x=60, y=90)
 
-    youtube_link_input_area = Entry(top, width=30).place(x=60, y=90)
-
-    submit_button = Button(top, text="Create Playlist").place(x=140, y=130)
-
+    submit_button = Button(top, text="Create Playlist", command=lambda: create_spotify_playlist(youtube_link_input_area))
+    submit_button.place(x=140, y=130)
+    top.mainloop()
+    submit_button.place(x=140, y=130)
     top.mainloop()
 
+def create_spotify_playlist(youtube_link_input_area):
+    youtube_url = youtube_link_input_area.get()
+
+    print("we got it: ", youtube_url)
+    #
+    # song_info = extract_song_info(youtube_url)
+    #
+    # search_and_add_to_playlist(song_info)
+
+    messagebox.showinfo("Success", "Playlist created on your Spotify")
+
+
+
+def main():
+    create_interface()
 
 if __name__ == "__main__":
     main()
